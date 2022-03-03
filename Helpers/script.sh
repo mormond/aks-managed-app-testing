b16265b7d4010b9e108a98b69287469284f3f679
Mike Ormond <mormond@microsoft.com>
2022-03-03 18:14:10 +0000

    Fix script helper

--- Helpers/script.sh
+++ Helpers/script.sh
@@ -1,11 +1,11 @@
 # Useful snippets of script
 
 # Generate ARM templace from Bicep file
-az bicep build --file ./Bicep/azuredeploy.bicep
+az bicep build --file ./Bicep/mainTemplate.bicep
 
 # Resource group deployment of ARM template
 RG="test-delete"
-if [[ $(az group exists --resource-group "$RG") == 'true' ]] ; then
+if [[ $(az group exists --resource-group "$RG") == 'false' ]]; then
     az group create --location "westeurope" --resource-group "$RG"
     az deployment group create --resource-group "$RG" --template-file "./Bicep/mainTemplate.json"
 else
